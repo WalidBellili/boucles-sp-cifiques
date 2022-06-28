29a07413d9781252325251566d7d272385449762
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-06-28 14:03:39 +0200

    exo2 changed object

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-const array = [1, 2, 3, 4, 5]
+// exo1
+// const array = [1, 2, 3, 4, 5]
 
-const double = array.map((multiplication) => {
-    console.log(multiplication);
-    return multiplication * 2
+// const double = array.map((multiplication) => {
+//     console.log(multiplication);
+//     return multiplication * 2
     
-})
-console.log(double);
+// })
+// console.log(double);
+
+// exo2
+
+const longNames = [
+	{
+		firstName: "Jane",
+		lastName: "Doe"
+	},
+	{
+		firstName: "John",
+		lastName: "Smith"
+	}
+]
+const shortNames = longNames.map((fullName) => {
+    return fullName= fullName.firstName
+}) 
+console.log(shortNames);
